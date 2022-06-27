1ba03c6edd266fe8ef6d414e7cc2f89dc379194e
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-06-27 11:43:31 +0700

    Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -37,35 +39,50 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $credentials = [
-            'email' => $request->email,
-            'password' => $request->password,
-        ];
+        $validate = Validator::make([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
-        // dd($credentials);
-        if ($credentials['email'] == null && $credentials['password']) {
-            return response()->json(["message" => "Gagal"]);
+        if ($validate->fails()) {
+            $response = [
+                'success' => false,
+                'message' => 'Validator error',
+                'error' => $validate->errors()
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+        } else {
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                $response = [
+                    'success' => false,
+                    'message' => 'Unauthorized',
+                ];
+
+                return response()->json($response, Response::HTTP_UNAUTHORIZED);
+            }
+
+            $user = User::where('email', $request->email)->firstOrFail();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new Exception('Error in login');
+            }
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()
+                ->json(['message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer',]);
         }
-
-        $user = User::where('email', $request->email)->firstOrFail();
-
-        if (!Hash::check($user->password, $request->password)) {
-            dd($user);
-        }
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()
-            ->json(['message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer',]);
     }
 
     // method for user logout and delete token
     public function logout()
     {
-        // auth()->user()->tokens()->delete();
+        auth()->user()->tokens()->delete();
 
-        // return [
-        //     'message' => 'You have successfully logged out and the token was successfully deleted'
-        // ];
+        return [
+            'message' => 'You have successfully logged out and the token was successfully deleted'
+        ];
     }
 }
